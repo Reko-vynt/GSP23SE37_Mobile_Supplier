34fccac92722669595ff085ae756f24f5134b6e9
Reko-vynt <nguyentanvy636@gmail.com>
2023-01-03 23:40:04 +0700

    fisrt design homepage

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -10,6 +10,55 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.topRight,
+                    colors: [Colors.orange, Colors.red])),
+            width: double.infinity,
+            height: 150,
+            child: const Text('Hello'),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                height: 200,
+                width: 100,
+                margin: const EdgeInsets.only(top: 50),
+                color: Colors.blueAccent,
+              ),
+              Wrap(alignment: WrapAlignment.start, children: <Widget>[
+                Container(
+                  width: 150,
+                  height: 150,
+                  color: Colors.blue,
+                ),
+                Container(
+                  width: 150,
+                  height: 150,
+                  color: Colors.red,
+                ),
+                Container(
+                  width: 150,
+                  height: 150,
+                  color: Colors.black,
+                ),
+                Container(
+                  width: 150,
+                  height: 150,
+                  color: Colors.yellow,
+                ),
+              ]),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
